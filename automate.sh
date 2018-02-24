52e81b9d3d63d4e6b8c950d79a972e79416cc1aa
bijan <bijan.r.rofoee@gmail.com>
2018-02-24 17:26:55 +0000

    commit $date

--- automate.sh
+++ automate.sh
@@ -6,7 +6,6 @@ echo 'time is $NOW'
 
 echo 'your commit message?'
 read COMMITMESSAGE
-echo $COMMITMESSAGE
 
 read -s -p "Enter GIT Password: " mypassword
 
@@ -14,8 +13,8 @@ echo 'Git work now'
 git add .
 git commit -m 'commit $date'
 git push
-expect \"ser"\
+expect "ser"
 send $GUSER
-expect \"assword:\"
-send \"$mypassword\r\"
+expect "assword"
+send "$mypassword\r"
 echo 'Git is updated'
